6e9498fc0c698c8a16b42e909cbcf86631adb7c6
habib0235 <habib.alhabib@esprit.tn>
2026-01-09 02:28:29 +0100

    Add application properties, update main class, and enhance pom.xml dependencies

--- hospitalerp-boot/src/main/java/com/example/hospitalerp/HospitalErpApplication.java
+++ hospitalerp-boot/src/main/java/com/example/hospitalerp/HospitalErpApplication.java
@@ -2,8 +2,13 @@ package com.example.hospitalerp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.example.hospitalerp",
+        "com.example.hospitalerp.controller"
+})
 public class HospitalErpApplication {
 
     public static void main(String[] args) {
